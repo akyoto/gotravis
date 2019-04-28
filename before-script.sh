2deb1455c479f688ffa2069df67d144d4b0f7989
Eduard Urbach <e.urbach@gmail.com>
2019-04-28 13:53:28 +0900

    Enable Go modules

--- before-script.sh
+++ before-script.sh
@@ -3,6 +3,9 @@
 # Exit on error
 set -e
 
+# Enable Go modules
+export GO111MODULE=on
+
 # Use a fixed version of golangci-lint to get reproducible tests
 GOLANGCILINTVERSION=1.16.0
 
@@ -10,4 +13,4 @@ GOLANGCILINTVERSION=1.16.0
 curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(go env GOPATH)/bin v$GOLANGCILINTVERSION
 
 # Build dependencies
-go get -t -v ./...
+go get -v ./...
